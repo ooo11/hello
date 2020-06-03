99e62201ed48340f8c4fefdcf5016a0f70861c4b
Mark Mizuro <markmizuro@Marks-MacBook-Air.local>
2020-06-03 15:07:40 +0800

    new hello

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const routes = require('./routes/index');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(express.static('public'));
+// Static
+
+app.use('/', routes);
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,18 @@
+const auth = require('http-auth');
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const { check, validationResult } = require('express-validator');
+
+const router = express.Router();
+const basic = auth.basic({
+    file: path.join(__dirname, '../users.htpasswd')
+});
+
+router.get('/', auth.connect(basic), (req, res) => {
+    res.render('index', { title: 'Search parts', message: 'Hello there!' });
+});
+
+
+
+module.exports = router;
