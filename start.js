5fac615cc7598f6c4e6f4035eb6a25bc24e15757
ooo11 <markmizuro@gmail.com>
2020-06-06 11:35:53 +0800

    make it better

--- start.js
+++ start.js
@@ -1,21 +1,21 @@
-require('dotenv').config({ path: 'process.env' });
-const mongoose = require('mongoose');
+// require('dotenv').config({ path: 'process.env' });
+// const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DATABASE, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true
-});
+// // mongoose.connect(process.env.DATABASE, {
+// // 	useNewUrlParser: true,
+// // 	useUnifiedTopology: true
+// // });
 
-mongoose.connection
-	.on('open', () => {
-		console.log('Mongoose connection open');
-	})
-	.on('error', (err) => {
-		console.log(`Connection error: ${err.message}`);
-	});
+// // mongoose.connection
+// // 	.on('open', () => {
+// // 		console.log('Mongoose connection open');
+// // 	})
+// // 	.on('error', (err) => {
+// // 		console.log(`Connection error: ${err.message}`);
+// 	});
 
 const app = require('./app');
 
-const server = app.listen(3000, () => {
+const server = app.listen(5000, () => {
 	console.log(`Express is running on port ${server.address().port}`);
 });
