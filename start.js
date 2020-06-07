cf88c875a449cc1404e04d33bff93cc2bedbf340
ooo11 <markmizuro@gmail.com>
2020-06-07 10:50:57 +0800

    new commit

--- start.js
+++ start.js
@@ -18,7 +18,7 @@
 
 const app = require('./app');
 
-const server = app.listen(5000, () => {
+const server = app.listen(process.env.PORT || 5000, () => {
 	console.log(`Express is running on port 5000`);
 });
 
